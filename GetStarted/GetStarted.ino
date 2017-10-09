ea026b893c3a3b85fee13010ccfabe96c1b310e2
Jan Enning <enning@kleinejan.org>
2017-10-09 20:29:33 +0200

    Add files via upload

---
+++ GetStarted/GetStarted.ino
@@ -0,0 +1,149 @@
+/***************************************************
+  DFPlayer - A Mini MP3 Player For Arduino
+  <https://www.dfrobot.com/index.php?route=product/product&product_id=1121>
+
+ ***************************************************
+  This example shows the basic function of library for DFPlayer.
+
+  Created 2016-12-07
+  By [Angelo qiao](Angelo.qiao@dfrobot.com)
+
+  GNU Lesser General Public License.
+  See <http://www.gnu.org/licenses/> for details.
+  All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+  1.Connection and Diagram can be found here
+  <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+  2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ****************************************************/
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+#include <NewPing.h>
+
+int sensorPin = A3;
+
+#define TRIGGER_PIN  8  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     9  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+SoftwareSerial mySoftwareSerial(4, 3); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while (true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
+}
+
+void loop()
+{
+  static unsigned int sensorValue = analogRead(sensorPin);
+  static unsigned long timer = millis();
+  static unsigned int volume = map(sensorValue, 0, 1023, 0, 30);
+
+  Serial.println("Volume:");
+  Serial.println(analogRead(sensorPin));
+  if (millis() - timer > 1000) {
+    Serial.println("Beep!");
+    timer = millis();
+
+  }
+  unsigned int uS = sonar.ping_cm();
+  Serial.println("CM:");
+  Serial.println(uS);
+  if (uS > 100 && uS < 130 ) {
+    //    Serial.println(uS);
+    //    Serial.println("trigger!");
+    if (myDFPlayer.available()) {
+      if (myDFPlayer.readType() == 'DFPlayerPlayFinished') {
+        myDFPlayer.volume(volume);
+        myDFPlayer.play(4);
+
+      } else {
+        myDFPlayer.volume(volume);
+        myDFPlayer.play(4);
+
+      }
+    }
+  }
+
+  delay(150);
+}
+
+void printDetail(uint8_t type, int value) {
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      //      myDFPlayer.play(1);  //Play the first mp3
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+
+}
